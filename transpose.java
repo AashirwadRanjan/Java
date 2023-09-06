ac254b2f1cc00724f550d582266450d9cad75726
AashirwadRanjan <85273995+AashirwadRanjan@users.noreply.github.com>
2023-09-06 23:57:16 +0530

    Update transpose.java

--- transpose.java
+++ transpose.java
@@ -2,9 +2,11 @@ class transpose{
     public static void main(String[]args){
         int temp[][] = new int[3][3];
         int arr[][]= {
-            {1,2,3},{4,5,6},{7,8,9}
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
         };
-        System.out.print("The matrix is:");
+        System.out.println("The matrix is:");
 
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[0].length;j++){
@@ -18,7 +20,7 @@ class transpose{
             for(int j=i;j<arr[0].length;j++){
                 temp[i][j]=arr[i][j];
                 arr[i][j]=arr[j][i];
-                arr[i][j]=temp[i][j];
+                arr[j][i]=temp[i][j];
                 
             }
 
@@ -26,7 +28,7 @@ class transpose{
         
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[0].length;j++){
-                System.out.println(arr[i][j]);
+                System.out.print(arr[i][j]);
             }
         }
         
