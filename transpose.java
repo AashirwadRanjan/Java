d7a1248abe824de5685a6abd5d8c0c324b413f5e
AashirwadRanjan <85273995+AashirwadRanjan@users.noreply.github.com>
2023-09-07 00:46:41 +0530

    Update transpose.java

--- transpose.java
+++ transpose.java
@@ -10,8 +10,9 @@ class transpose{
 
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[0].length;j++){
-                System.out.print(arr[i][j]);
+                System.out.print(arr[i][j]+" ");
             }
+            System.out.println("");
         }
         System.out.println();
         System.out.println("Resulted matrix is:");
@@ -26,10 +27,11 @@ class transpose{
 
         }
         
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                System.out.print(arr[i][j]);
+        for(int c[]:arr){
+            for(int d:c){
+                System.out.print(d+" ");
             }
+            System.out.println("");
         }
         
 
