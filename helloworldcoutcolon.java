082b2ec3677063a797df6aad398220c37ba72c12
AashirwadRanjan <85273995+AashirwadRanjan@users.noreply.github.com>
2023-09-07 23:44:16 +0530

    Add files via upload

---
+++ helloworldcoutcolon.java
@@ -0,0 +1,9 @@
+public class helloworldcoutcolon {
+    public static void main(String []args){
+        //if(System.out.printf("Helloworld")== null){}
+        //if(System.out.append("Hello world")==null){}
+        if(System.out.append("hello wolrd").equals(null)){}
+        
+    }
+    
+}
